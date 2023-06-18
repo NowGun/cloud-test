2eacac729eb6b6fce0302824e9960095fec24383
Daniil Zhirov <nowgun98@gmail.com>
2023-06-18 18:43:35 +0300

    Внесены корректировки в index

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import './_index.scss';
 import reportWebVitals from './reportWebVitals';
+import Card from "./components/component/card/Card";
+import Routing from "./components/pages/Routing";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <Card>
+            <Routing/>
+        </Card>
+    </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
