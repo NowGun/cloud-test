207bd2a792953344e3409afd57befa69cf593fe9
Daniil Zhirov <nowgun98@gmail.com>
2023-06-18 18:37:10 +0300

    Внесены изменения в компонент Input

--- src/components/component/input/Input.jsx
+++ src/components/component/input/Input.jsx
@@ -1,10 +1,65 @@
 import './_input.scss'
+import MaskedInput from 'react-text-mask';
 
-export default function Input({text, textType, type, placeholder}) {
+const phoneNumberMask = [
+    '+',
+    '7',
+    ' ',
+    '(',
+    /[1-9]/,
+    /\d/,
+    /\d/,
+    ')',
+    ' ',
+    /\d/,
+    /\d/,
+    /\d/,
+    '-',
+    /\d/,
+    /\d/,
+    '-',
+    /\d/,
+    /\d/,
+];
+
+const Input = ({field, form, disabled, ...props}) => {
+    const {name} = field;
+    const {touched, errors, setFieldValue} = form;
+
+    const handleChange = (e) => {
+        const {value} = e.target;
+        setFieldValue(name, value);
+    };
+
+    const handleClear = () => {
+        setFieldValue(name, '');
+    };
     return (
-        <div className={textType}>
-            <p>{text}</p>
-            <input type={type} placeholder={placeholder}/>
+        <div className="input">
+            {'' === 'tel' ? (
+                <MaskedInput
+                    mask={phoneNumberMask}
+                    placeholder="+7 (999) 999-99-99"
+                    guide={false}
+                    showMask={false}
+                    disabled={disabled}
+                    {...field}
+                    {...props}
+                />
+            ) : (
+                <input {...field} {...props} disabled={disabled} onChange={handleChange}
+                />
+            )}
+
+            {
+                field.value && !disabled && (
+                    <button type="button" onClick={handleClear}>
+                        <i className="icon--close"/>
+                    </button>
+                )
+            }
         </div>
     );
-}
+};
+
+export default Input;
